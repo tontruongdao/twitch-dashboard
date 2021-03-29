df58c52731db01cb2b6b1acadfbb5f90b8bd0572
Truong Dao <ton.truong.dao@outlook.com>
2021-03-29 12:58:18 -0400

    debugged client

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
 
-// React.useEffect(()=> {
-//   fetch("/api/sayHello").then(res => res.json()).then(json => console.log(json));
-// },[])
-
 const App = () => {
 
   // React States
   const [ loading, setLoading ] = React.useState(false)
-  const [ games, setGames ] = React.useState([])
+  const [ games, setGames ] = React.useState("")
+  const [ chessData, setChessData ] = React.useState("")
   
   // Game Data
-  let chessData = null;
   let dotaData = null;
   let heartData = null
   let rocketData = null
@@ -25,45 +21,27 @@ const App = () => {
       .then(res => res.json())
         .then(json => {
           setGames(json);
+
+          setChessData(games[0])
           setLoading(false)
-        });
+        })
+    //  console.log(games) 
   },[])
 
   // Updating game data
-  if (games) {
-    chessData = games.find(game => game.id === "743")
-    dotaData = games.find(game => game.id === "30921")
-    heartData = games.find(game => game.id === "138585")
-    rocketData = games.find(game => game.id === "30921")
-    console.log(chessData, dotaData, heartData, rocketData)
+  // if (games) {
+  //   console.log(chess, dotaData, heartData, rocketData)
+  // }
+
+  if(loading){
+    console.log(`loading ${games}`)
   }
 
   return (
     <div>
       <h1> Truong Dashboard Project</h1>
-      {!loading ? (
-        <div>
-          {/* Socket */}
-          <div>
-            <h2>Socket Data</h2>
-            <div>
-              <div>{chessData.name}</div>
-              <div>Views: <span>{chessData.total_views}</span></div>
-            </div>
-          </div>
-          {/* Victory */}
-          <div>
-            <h3> Line Chart </h3>
-            <div>
-              <div>{dotaData.name}</div>
-              <div>Views: <span>{dotaData.total_views}</span></div>
-              <div>{heartData.name}</div>
-              <div>Views: <span>{heartData.total_views}</span></div>
-              <div>{rocketData.name}</div>
-              <div>Views: <span>{rocketData.total_views}</span></div>
-            </div>
-          </div>
-        </div>
+      {!loading && games ? (
+      <div> game name: {games[0].name}</div> 
       ) : (
       <div>Loading...</div>
       )}
