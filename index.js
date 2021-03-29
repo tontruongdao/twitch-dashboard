b957d11d0a6cde18b79ab276fc9c53df703f7bfb
Truong Dao <ton.truong.dao@outlook.com>
2021-03-29 11:46:57 -0400

    test

--- index.js
+++ index.js
@@ -51,13 +51,8 @@ app
     }
   })
 
-  if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  }
+  .get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  })
 
   app.listen(PORT, () => console.log(`listening on port: ${PORT}`));
